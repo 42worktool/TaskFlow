91ca888292df17da376b0bff25f6f1ee226af47f
KHR416 <hyy741@naver.com>
2026-06-18 14:03:37 +0900

    chore: add types for initial UI

---
+++ frontend/src/type.ts
@@ -0,0 +1,42 @@
+export type User = {
+  id: string
+  name: string
+  email: string
+}
+
+export type WorkspaceMember = {
+  id: string
+  workspaceId: string
+  userId: string
+  role: 'OWNER' | 'MEMBER'
+}
+
+export type Workspace = {
+  id: string
+  title: string
+}
+
+export type Board = {
+  id: string
+  title: string
+  workspaceId: string
+}
+
+export type BoardList = {
+  id: string
+  boardId: string
+  title: string
+  order: number
+}
+
+export type CardLocation = { type: 'list'; listId: string } | { type: 'inbox'; userId: string }
+
+export type Card = {
+  id: string
+  location: CardLocation
+  title: string
+  description: string
+  order: number
+  assigneeIds: string[]
+  dueDate?: string
+}
