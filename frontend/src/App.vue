e138106eb3a8d0c4d47469d73e6f6627fae2e64f
KHR416 <hyy741@naver.com>
2026-06-29 10:05:26 +0900

    chore: reset

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import Board from './components/Board.vue'
-import LoginPage from './components/LoginPage.vue'
-import type { User } from './type'
+<script setup lang="ts"></script>
 
-const currentUser = ref<User | null>(null)
-
-function handleLogin(user: User) {
-  currentUser.value = user
-}
-
-function handleLogout() {
-  currentUser.value = null
-}
-</script>
-
-<template>
-  <LoginPage v-if="!currentUser" @login="handleLogin" />
-  <Board v-else :current-user="currentUser" @logout="handleLogout" />
-</template>
+<template></template>
